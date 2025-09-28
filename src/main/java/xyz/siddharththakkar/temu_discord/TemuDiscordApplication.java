1d12c69c86499d6fff199559deb814e6e71cd503
woustachemax <sid2011thakkar@gmail.com>
2025-09-27 17:14:41 -0700

    init

---
+++ src/main/java/xyz/siddharththakkar/temu_discord/TemuDiscordApplication.java
@@ -0,0 +1,13 @@
+package xyz.siddharththakkar.temu_discord;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemuDiscordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemuDiscordApplication.class, args);
+	}
+
+}
---
+++ src/test/java/xyz/siddharththakkar/temu_discord/TemuDiscordApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.siddharththakkar.temu_discord;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemuDiscordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
